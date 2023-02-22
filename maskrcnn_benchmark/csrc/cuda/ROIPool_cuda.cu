f3957a27a308b7c5c58c4a42f807187e951e68b7
Ravisri Valluri <70142856+RavisriVK@users.noreply.github.com>
2023-02-22 13:52:29 +0530

    Update ROIPool_cuda.cu

--- maskrcnn_benchmark/csrc/cuda/ROIPool_cuda.cu
+++ maskrcnn_benchmark/csrc/cuda/ROIPool_cuda.cu
@@ -2,10 +2,13 @@
 #include <ATen/ATen.h>
 #include <ATen/cuda/CUDAContext.h>
 
-#include <THC/THC.h>
-#include <THC/THCAtomics.cuh>
-#include <THC/THCDeviceUtils.cuh>
-
+// #include <THC/THC.h>
+// #include <THC/THCAtomics.cuh>
+#include <ATen/cuda/Atomic.cuh>
+// #include <THC/THCDeviceUtils.cuh>
+#include <ATen/cuda/DeviceUtils.cuh>
+#include <ATen/ceil_div.h>
+#include <ATen/cuda/ThrustAllocator.h>
 
 // TODO make it in a common file
 #define CUDA_1D_KERNEL_LOOP(i, n)                            \
@@ -126,11 +129,13 @@ std::tuple<at::Tensor, at::Tensor> ROIPool_forward_cuda(const at::Tensor& input,
 
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  dim3 grid(std::min(THCCeilDiv((long)output_size, 512L), 4096L));
+  dim3 grid(std::min(at::ceil_div((long)output_size, 512L), 4096L));
+  // dim3 grid(std::min(THCCeilDiv((long)output_size, 512L), 4096L));
   dim3 block(512);
 
   if (output.numel() == 0) {
-    THCudaCheck(cudaGetLastError());
+    c10::cuda::CUDACachingAllocator::raw_alloc(cudaGetLastError());
+    // THCudaCheck(cudaGetLastError());
     return std::make_tuple(output, argmax);
   }
 
@@ -148,7 +153,8 @@ std::tuple<at::Tensor, at::Tensor> ROIPool_forward_cuda(const at::Tensor& input,
          output.data<scalar_t>(),
          argmax.data<int>());
   });
-  THCudaCheck(cudaGetLastError());
+  c10::cuda::CUDACachingAllocator::raw_alloc(cudaGetLastError());
+  // THCudaCheck(cudaGetLastError());
   return std::make_tuple(output, argmax);
 }
 
@@ -173,12 +179,14 @@ at::Tensor ROIPool_backward_cuda(const at::Tensor& grad,
 
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  dim3 grid(std::min(THCCeilDiv((long)grad.numel(), 512L), 4096L));
+  dim3 grid(std::min(at::ceil_div((long)grad.numel(), 512L), 4096L));
+  // dim3 grid(std::min(THCCeilDiv((long)grad.numel(), 512L), 4096L));
   dim3 block(512);
 
   // handle possibly empty gradients
   if (grad.numel() == 0) {
-    THCudaCheck(cudaGetLastError());
+    c10::cuda::CUDACachingAllocator::raw_alloc(cudaGetLastError());
+    // THCudaCheck(cudaGetLastError());
     return grad_input;
   }
 
@@ -197,6 +205,7 @@ at::Tensor ROIPool_backward_cuda(const at::Tensor& grad,
          grad_input.data<scalar_t>(),
          rois.contiguous().data<scalar_t>());
   });
-  THCudaCheck(cudaGetLastError());
+  c10::cuda::CUDACachingAllocator::raw_alloc(cudaGetLastError());
+  // THCudaCheck(cudaGetLastError());
   return grad_input;
 }
